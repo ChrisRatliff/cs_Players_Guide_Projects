63a63bd8b007a1a618c22633e1196491efe3d7cd
ChrisRatliff <110771060+ChrisRatliff@users.noreply.github.com>
2023-08-27 12:54:11 -0500

    Add files via upload

---
+++ Bread/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Xml.Linq;
+
+Console.WriteLine("Bread is ready");
+string name;
+Console.WriteLine("Who is the bread for?");
+name = Console.ReadLine();
+Console.WriteLine("Noted: " + name + " got bread");
